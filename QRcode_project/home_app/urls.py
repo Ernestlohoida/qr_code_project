4fa5d6784b73a59b93edfd1ec4a719c2f228d3a9
Ernestlohoida <ernestlogoyda99@gmail.com>
2025-02-01 17:04:28 +0200

    create new app

---
+++ QRcode_project/home_app/urls.py
@@ -0,0 +1,7 @@
+from .views import *
+
+from django.urls import path
+
+urlpatterns = [
+    path('', render_home, name= "home")
+]
---
+++ QRcode_project/home_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def render_home(request):
+    return render(request=request, template_name='home/home.html')
+
+
+
+# Create your views here.
